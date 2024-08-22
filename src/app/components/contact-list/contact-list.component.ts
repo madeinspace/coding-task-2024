e39e1c84060579f50e80d1549dfc8766932db66a
madeinspace <fabrice@funkadelik.com>
2024-08-22 11:16:14 +1000

    Feat: added contact loading message

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Contact } from 'src/app/models/contact.model';
 import { ContactService } from 'src/app/services/contact.service';
 import { State, selectActiveContact, selectContactList } from '../../state';
 import * as actions from '../../state/actions';
-
 
 @Component({
   selector: 'app-contact-list',
@@ -16,22 +16,26 @@ export class ContactListComponent {
 
   contactList$: Observable<Contact[]>;
   activeContact$: Observable<Contact | undefined>;
+  isLoading$: Observable<boolean>;
 
   constructor(
     private contactService : ContactService,
     private store : Store<State>
   ){
-    this.contactList$ = this.store.select(selectContactList)
+    this.contactList$ = this.store.select(selectContactList);
     this.activeContact$ = this.store.select(selectActiveContact);
+
+    this.isLoading$ = this.contactList$.pipe(
+      map(contacts => contacts.length === 0)
+    );
   }
 
   viewContactClicked(contactId : number ){
-    this.store.dispatch(actions.contactSelected({contactId}))
+    this.store.dispatch(actions.contactSelected({contactId}));
   }
 
   editContactClicked(contact : Contact){
-    this.store.dispatch(actions.editContactClicked({contact}))
+    this.store.dispatch(actions.editContactClicked({contact}));
   }
-
 
 }
